d740f447912a57f3cc51c84a922be6c3e8a8170e
ajay <ajayrajput9554@gmail.com>
2023-02-04 19:55:45 +0530

    completed Response object

---
+++ block-BNaaap/server.js
@@ -0,0 +1,13 @@
+let http=require('http')
+
+let server=http.createServer(handleResponse)
+function handleResponse(req,res){
+    console.log(req.method)
+    res.status=201
+    res.setHeader('content-Type','text/html')
+    res.write('hello')
+    res.end('<h1>Welcome</h1>')
+}
+server.listen(4444,()=>{
+    console.log('server is listening to port 4444')
+})
